20b5ca5d9e0a5914bb07dd4f65ffba0dfecb2a70
rajkumarpalani <prajkumar.cse@gmail.com>
2021-02-22 14:32:21 +0530

    Add samples for API error handling, update readme to reflect the same

--- samples/cancel_payout_item.py
+++ samples/cancel_payout_item.py
@@ -6,6 +6,8 @@ from create_payouts import CreatePayouts
 from get_payouts import GetPayouts
 from get_payout_item import GetPayoutItem
 from paypalhttp.serializers.json_serializer import Json
+from paypalhttp.http_error import HttpError
+from paypalhttp.encoder import Encoder
 
 class CancelPayoutItem(PayPalClient):
 
@@ -14,22 +16,36 @@ class CancelPayoutItem(PayPalClient):
      Upon cancelling the item status becomes RETURNED and the funds returned back to the sender"""
     def cancel_payout_item(self, payout_item_id, debug=False):
         request = PayoutsItemCancelRequest(payout_item_id)
-        response = self.client.execute(request)
 
-        if debug:
-            print("Status Code: ", response.status_code)
-            print("Payout Item ID: " + response.result.payout_item_id)
-            print("Payout Item Status: " + response.result.transaction_status)
-            print("Links: ")
-            for link in response.result.links:
-                print('\t{}: {}\tCall Type: {}'.format(
-                    link.rel, link.href, link.method))
+        try:
+            response = self.client.execute(request)
 
-            # To toggle print the whole body comment/uncomment the below line
-            #json_data = self.object_to_json(response.result)
-            #print "json_data: ", json.dumps(json_data, indent=4)
+            if debug:
+                print("Status Code: ", response.status_code)
+                print("Payout Item ID: " + response.result.payout_item_id)
+                print("Payout Item Status: " + response.result.transaction_status)
+                print("Links: ")
+                for link in response.result.links:
+                    print('\t{}: {}\tCall Type: {}'.format(
+                        link.rel, link.href, link.method))
 
-        return response
+                # To toggle print the whole body comment/uncomment the below line
+                #json_data = self.object_to_json(response.result)
+                #print "json_data: ", json.dumps(json_data, indent=4)
+
+            return response
+        except HttpError as httpe:
+            # Handle server side API failure
+            encoder = Encoder([Json()])
+            error = encoder.deserialize_response(httpe.message, httpe.headers)
+            print("Error: " + error["name"])
+            print("Error message: " + error["message"])
+            print("Information link: " + error["information_link"])
+            print("Debug id: " + error["debug_id"])
+
+        except IOError as ioe:
+            #Handle cient side connection failures
+            print(ioe.message)
 
 
 """This is the driver function which invokes the cancel_payout_item function to cancel an UNCLAIMED payout item."""
@@ -39,4 +55,6 @@ if __name__ == "__main__":
     get_response = GetPayouts().get_payouts(batch_id, True)
     item_id = get_response.result.items[0].payout_item_id
     time.sleep(20)
+    CancelPayoutItem().cancel_payout_item(item_id, True)
+    print("Cancel already cancelled item - " + item_id)
     CancelPayoutItem().cancel_payout_item(item_id)
