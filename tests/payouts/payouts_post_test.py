3cefba2e87d1605b786eb5779f1458ed507da970
Cheng Chang <chengchang@paypal.com>
2021-04-30 17:34:23 -0700

    add logging

--- tests/payouts/payouts_post_test.py
+++ tests/payouts/payouts_post_test.py
@@ -12,7 +12,6 @@ class PayoutsPostTest(TestHarness):
 
         sys.stdout.write('PayoutsPostTest')
         sys.stdout.write(response.headers)
-        sys.stdout.write(response.headers.getheader('content-type'))
         sys.stdout.write(response.msg)
         sys.stdout.flush()
 
